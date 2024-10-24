5d351731b768d24985d4fe962406803380373e13
Matheus Miranda <77033743+BigMiranda@users.noreply.github.com>
2024-10-24 20:59:54 -0300

    FIX/ main.py ajustada para reconsiderar attribute_mapping_modules

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import json
 from src.server.instance import app
 from src.controllers.data_extraction_controller import register_extraction_routes
 from src.controllers.health_check_controller import register_health_routes
-from src.controllers.attribute_mapping_controller import register_attribute_mapping_routes
 from src.controllers.data_preprocessing_controller import register_preprocessing_routes
+from src.controllers.attribute_mapping_controller import register_attribute_mapping_routes  # Importação adicionada
 from flasgger import Swagger
 
 # Carregar as configurações do arquivo config.json
@@ -31,8 +31,8 @@ swagger = Swagger(app, config={
 # Registrar rotas das controllers
 register_health_routes(app)
 register_extraction_routes(app)
-register_attribute_mapping_routes(app)
 register_preprocessing_routes(app)
+register_attribute_mapping_routes(app)
 
 # Exemplo de acesso às variáveis de configuração carregadas
 sth_comet_host = config['sth_comet_host']
@@ -42,10 +42,8 @@ orion_context_broker_port = config['orion_context_broker_port']
 service = config['fiware_service']
 service_path = config['fiware_service_path']
 
-# Agora, essas variáveis podem ser usadas em qualquer lugar do código
 print(f"STH Comet Host: {sth_comet_host}")
 print(f"Orion Context Broker Host: {orion_context_broker_host}")
 
 if __name__ == '__main__':
-    # Inicia o servidor Flask
     app.run(debug=True)
